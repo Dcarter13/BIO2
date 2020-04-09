fa3f4e626276cb71ddb0f7b36b3d5695015bb920
Dcarter13 <61527680+Dcarter13@users.noreply.github.com>
2020-04-08 17:08:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,22 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-
+var getAllRecords = function() {
+  $.getJSON(
+    "https://airtable.com/tblNDHP8o9smfQNHk/viwWIaK0laxzxru1Y?blocks=hide",
+    function(airtable) {
+      var html = [];
+      $.each(airtable.records, function(index, record) {
+        var id = record.id;
+        var image = record.fields["attack"];
+        var damage = record.fields["damage"];
+        var startup = record.fields["startup frame"];
+        html.push(listView(id, image, damage, startup));
+      });
+      $(".list-view").append(html);
+    }
+  );
+};
 
 
 
